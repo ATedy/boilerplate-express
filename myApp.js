3097789a81494e6dddff2ea0339899f521c39a70
Amanuel <weditedi25@gmail.com>
2021-05-06 14:33:46 +0100

    initial commit and package installation

--- myApp.js
+++ myApp.js
@@ -1,39 +1,9 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
+app.get("/", function (req, res) {
+  res.send("Hello, world!");
+  console.log("Hello world");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
