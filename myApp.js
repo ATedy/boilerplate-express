817f67e4bda2b4ee77fde08dde05a8537684e21d
Amanuel <weditedi25@gmail.com>
2021-05-06 16:09:50 +0100

    Task-5  json route

--- myApp.js
+++ myApp.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var app = express();
-// const path = require("path");
+var path = require("path");
 
 app.get("/", function (req, res) {
-  // res.sendFile(path.join(__dirname, "../views", "index.html"));
   res.sendFile(__dirname + "/views/index.html");
+});
+
+// app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// serving the app with a json router
+
+app.get("/json", function (req, res) {
+  res.send({message: "Hello json"});
 });
 
 module.exports = app;
